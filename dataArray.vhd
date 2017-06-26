dc5dd38d75875121af225d7eeaa640f9d2d68c7a
mahi97 <mohammadmahdi76@gmail.com>
2017-06-26 19:18:54 +0430

    add ignore and fix entity of cache and data array

--- dataArray.vhd
+++ dataArray.vhd
@@ -2,22 +2,25 @@
 -- Author:        Mohammad Mahdi Rahimi (mohammadmahdi76@gmail.com)
 --
 -- Create Date:   26-05-2017
--- Module Name:   Cache.vhd
+-- Module Name:   dataArray.vhd
 --------------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity Cache is
+entity dataArray is
   port (
 	clock   : in  std_logic;
-	address : in  std_logic_vector(15 downto 0);
-	data    : out std_logic_vector(31 downto 0);
-	hit     : out std_logic
+	address : in  std_logic_vector(5 downto 0);
+	wren    : in  std_logic;
+	wrdata  : in  std_logic_vector(31 downto 0);
+	data    : out std_logic_vector(31 downto 0)
   ) ;
-end entity ; -- Cache
+end entity ; -- dataArray
 
-architecture arch of Cache is
+architecture arch of dataArray is
+
+
 
 begin
 

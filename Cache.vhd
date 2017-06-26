d3885c2ac4943e74b5462410f36aae112794670a
mahi97 <mohammadmahdi76@gmail.com>
2017-06-26 18:57:53 +0430

    initial commit

---
+++ Cache.vhd
@@ -0,0 +1,25 @@
+--------------------------------------------------------------------------------
+-- Author:        Mohammad Mahdi Rahimi (mohammadmahdi76@gmail.com)
+--
+-- Create Date:   26-05-2017
+-- Module Name:   Cache.vhd
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity Cache is
+  port (
+	clock : in std_logic
+  ) ;
+end entity ; -- Cache
+
+architecture arch of Cache is
+
+
+
+begin
+
+
+
+end architecture ; -- arch
